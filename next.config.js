c7dce9ba6050096e58870cd5012c8e1b1f5dd75f
Philip Etter <uniqueuptopermutation@proton.me>
2024-04-08 17:02:17 -0700

    Stuff

--- next.config.js
+++ next.config.js
@@ -1,11 +1,9 @@
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
+  latex: true
 })
 
 module.exports = {
   ...withNextra(),
-  images: {
-    unoptimized: true,
-  },
 };
