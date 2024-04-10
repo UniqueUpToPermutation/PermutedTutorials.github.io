24588f2d963fbea3eea913480df1fe118d5105b0
Philip Etter <uniqueuptopermutation@proton.me>
2024-04-09 23:21:26 -0700

    Physics tutorials

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,13 +2,13 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span><h2>A Random Walk With Drift</h2></span>,
+  logo: <span><h2>Mathematics Tutorials</h2></span>,
   project: {
     link: 'https://github.com/UniqueUpToPermutation',
   },
   docsRepositoryBase: 'https://github.com/UniqueUpToPermutation',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'Mathematics Tutorials',
   },
 }
 
