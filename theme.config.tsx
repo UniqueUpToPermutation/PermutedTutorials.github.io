b511cf5f14337279a9ca9ee6125d8553df38eb23
Philip Allen Etter <paetter@meta.com>
2024-04-11 20:44:59 -0700

    Finished all tutorials

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>Mathematics Tutorials</span>,
+  logo: <span>Random Math Tutorials with Drift</span>,
   project: {
     link: 'https://github.com/UniqueUpToPermutation',
   },
